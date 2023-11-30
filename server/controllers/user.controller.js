6c66185a7212bdc7b1260f0a49ac609ec8ecb93b
Illia <ilyachernykan@gmail.com>
2023-11-30 10:03:42 +0200

    Add condition render

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,7 +3,6 @@ const { User } = require("../models");
 module.exports.createUser = async (req, res, next) => {
   try {
     const { body } = req;
-    console.log(body);
     const [user] = await User.find({ email: body.email });
     if (user) {
       return res.status(200).send({ data: user });
